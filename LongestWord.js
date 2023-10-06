274e845ea5315b7cebbc3413a702a39aaf546b47
Abdul Haseeb Ahmad Bashir <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2023-10-06 15:09:51 +0300

    Create LongestWord.js

---
+++ LongestWord.js
@@ -0,0 +1,34 @@
+
+const findLongestWordWithMostVowels = (sentence)=> {
+    const words = sentence.split(' ');
+    let longestWord = '';
+    let longestWordVowelCount = 0;
+  
+    for (const word of words) {
+      let wordLength = 0;
+      let vowelCount = 0;
+  
+      for (const char of word) {
+        if (/^[a-zA-Z]$/.test(char)) {
+          wordLength++;
+          if ('aeiouAEIOU'.includes(char)) {
+            vowelCount++;
+          }
+        }
+      }
+  
+      if (wordLength > longestWord.length ||
+          (wordLength === longestWord.length && vowelCount > longestWordVowelCount)) {
+        longestWord = word;
+        longestWordVowelCount = vowelCount;
+      }
+    }
+  
+    return longestWord;
+  }
+  
+  // Example usage:
+  const inputSentence = "Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers";
+  const longest = findLongestWordWithMostVowels(inputSentence);
+  console.log("The longest word with the most vowels is: " + longest);
+  
